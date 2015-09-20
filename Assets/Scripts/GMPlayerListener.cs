8e12ff4d8782dfbc8fcffe908da7b6c562ccb71d
S. Baker <baker.101@hotmail.com>
2015-09-19 23:16:25 -0700

    added particle effects and sprites

--- Assets/Scripts/GMPlayerListener.cs
+++ Assets/Scripts/GMPlayerListener.cs
@@ -5,19 +5,34 @@ public class GMPlayerListener : MonoBehaviour {
 
 	public Transform player;
 	public GameObject startingLoc;
+	public float maxPlayerVelocity = 10f;
 
 	private Vector3 startingLocVector;
+
+	private Rigidbody2D playerRB;
 
 	// Use this for initialization
 	void Start () {
 		startingLoc = GameObject.FindGameObjectWithTag("Spawn Start");
 		startingLocVector = startingLoc.transform.position;
+
+		playerRB = player.GetComponent<Rigidbody2D>();
+
+		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//simple reset
 		if (player.position.y < -20){
 			player.position = startingLocVector;
+			playerRB.velocity = new Vector2(0f, 0f);
+		}
+
+		//limit max speed
+		if (playerRB.velocity.y > maxPlayerVelocity)
+		{
+			playerRB.velocity = new Vector2(maxPlayerVelocity, maxPlayerVelocity);
 		}
 	}
 }
