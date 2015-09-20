9ee83496bdcf202b084c692d686e57d9190eae4f
S. Baker <baker.101@hotmail.com>
2015-09-20 12:40:29 -0700

    fine tuned animations and fixed conflicting sprites

--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -10,6 +10,10 @@ namespace UnityStandardAssets._2D
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
+		public float cameraScaleFactor = .25f;
+
+		private Camera cam;
+		private Vector2 playerVelocity;
 
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
@@ -22,12 +26,16 @@ namespace UnityStandardAssets._2D
             m_LastTargetPosition = target.position;
             m_OffsetZ = (transform.position - target.position).z;
             transform.parent = null;
+			cam = Camera.main;
         }
 
 
         // Update is called once per frame
         private void Update()
         {
+			//get player speed
+			//playerVelocity = new Vector2(target.GetComponent<Rigidbody2D>().velocity.x, target.GetComponent<Rigidbody2D>().velocity.y);
+
             // only update lookahead pos if accelerating or changed direction
             float xMoveDelta = (target.position - m_LastTargetPosition).x;
 
@@ -45,7 +53,11 @@ namespace UnityStandardAssets._2D
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
+			//move camera to new pos
             transform.position = newPos;
+
+			//resize viewport when player speeds up - so player can see nearby oncoming objects easier
+			cam.orthographicSize = 4;
 
             m_LastTargetPosition = target.position;
         }
