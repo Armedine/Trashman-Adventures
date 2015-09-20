d848a8fb53b89043eed3fd9821715f8eb0575e70
S. Baker <baker.101@hotmail.com>
2015-09-19 18:16:23 -0700

    adding animated trashcanman and updated scripts

---
+++ Assets/Scripts/GMPlayerListener.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GMPlayerListener : MonoBehaviour {
+
+	public Transform player;
+	public GameObject startingLoc;
+
+	private Vector3 startingLocVector;
+
+	// Use this for initialization
+	void Start () {
+		startingLoc = GameObject.FindGameObjectWithTag("Spawn Start");
+		startingLocVector = startingLoc.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.position.y < -20){
+			player.position = startingLocVector;
+		}
+	}
+}
